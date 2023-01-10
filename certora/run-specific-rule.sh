9b78d0ccd91755f8a549d6b26807cb8456e717b0
Melvillian <melvillevt@gmail.com>
2023-01-10 15:09:32 -0500

    test git pre-commit hook

--- certora/run-specific-rule.sh
+++
@@ -1,3 +0,0 @@
-SOLC_VERSION=0.7.6 certoraRun BordaBug2.sol:Borda --verify Borda:Borda.spec \
---rule "$1" \
---msg "$2"
--- certora/run.sh
+++
@@ -1,2 +0,0 @@
-SOLC_VERSION=0.7.6 certoraRun BordaBug3.sol:Borda --verify Borda:Borda.spec \
---msg "$1"
